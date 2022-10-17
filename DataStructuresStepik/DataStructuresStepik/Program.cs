1f799e7eb0edee3a5dc560a84de3902e25012d4e
AtomicBim <romangman90@gmail.com>
2022-10-17 13:55:01 +0500

    Start vew

--- DataStructuresStepik/DataStructuresStepik/Program.cs
+++ DataStructuresStepik/DataStructuresStepik/Program.cs
@@ -9,68 +9,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string[] incomingData = Console.ReadLine().Trim().Split(' ');
 
-            int N = Convert.ToInt32(incomingData[0]);//docks
-            int K = Convert.ToInt32(incomingData[1]);//compartment
-            int P = Convert.ToInt32(incomingData[2]);//maximum planted barrels
-
-            int barrelsQuantity = 0;
-            int maxBarrelsQuantity = 0;
-
-            //create compartment stacks
-            Stack<int>[] stacks = new Stack<int>[K];
-            for (int i = 0; i < K; i++)
-            {
-                stacks[i] = new Stack<int>();
-            }
-
-            for (int j = 0; j < N; j++)
-            {
-                string[] internalData = Console.ReadLine().Trim().Split(' ');
-
-                char procedure = Convert.ToChar(internalData[0]);
-                int compartment = Convert.ToInt32(internalData[1]);
-                int fuelType = Convert.ToInt32(internalData[2]);
-
-                switch (procedure)
-                {
-                    case '+':
-                        stacks[compartment - 1].Push(fuelType);
-                        barrelsQuantity++;
-                        break;
-
-                    case '-':
-                        if (stacks[compartment - 1].Count == 0 || stacks[compartment - 1].Peek() != fuelType)
-                        {
-                            Console.WriteLine("Error");
-                            return;
-                        }
-                        else
-                        {
-                            stacks[compartment - 1].Pop();
-                            barrelsQuantity--;
-                        }
-                        break;
-                }
-
-                if (barrelsQuantity > maxBarrelsQuantity)
-                {
-                    maxBarrelsQuantity = barrelsQuantity;
-                }
-
-                if (barrelsQuantity > P)
-                {
-                    Console.WriteLine("Error");
-                    break;
-                }
-            }
-            if (barrelsQuantity != 0)
-            {
-                Console.WriteLine("Error");
-                return;
-            }
-            Console.WriteLine(maxBarrelsQuantity);
         }
     }
 }
