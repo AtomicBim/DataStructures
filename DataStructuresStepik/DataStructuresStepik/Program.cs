a873b840e88b5c130b403a255804e6d9b73b6f17
AtomicBim <romangman90@gmail.com>
2022-10-18 09:38:28 +0500

    Task solved using stack and list. List calculates cumulative totals of each digit in stack. Afterwards cunnig algorithm (lines 45 to 53) calculates sum of asked patient's hemoglobin.

--- DataStructuresStepik/DataStructuresStepik/Program.cs
+++ DataStructuresStepik/DataStructuresStepik/Program.cs
@@ -9,7 +9,55 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int n = Convert.ToInt32(Console.ReadLine());
 
+            Stack<long> stack = new Stack<long>();
+            List<long> list = new List<long>();
+            List<long> res = new List<long>();
+            long digit = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string data = Console.ReadLine();
+
+                switch (data[0])
+                {
+                    case '+':
+                        digit = Convert.ToInt64(data.Substring(1, data.Length - 1));
+                        stack.Push(digit);
+                        if (list.Count == 0)
+                        {
+                            list.Add(digit);
+                        }
+                        else
+                        {
+                            list.Add(list.Last() + digit);
+                        }
+                        break;
+
+                    case '-':
+                        res.Add(stack.Pop());
+                        list.RemoveAt(list.Count - 1);
+                        break;
+
+                    case '?':
+                        digit = Convert.ToInt64(data.Substring(1, data.Length - 1));
+                        long peek = list.Last();
+                        if (list.Count == 1 || digit == list.Count)
+                        {
+                            res.Add(peek);
+                        }
+                        else
+                        {
+                            res.Add(peek - list[list.Count - (int)digit - 1]);
+                        }
+                        break;
+                }
+            }
+            foreach (var item in res)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
