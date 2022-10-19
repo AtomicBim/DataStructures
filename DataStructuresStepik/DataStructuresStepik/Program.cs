73d43a61ac956cd9522cd0da3ce4b947a747e958
AtomicBim <romangman90@gmail.com>
2022-10-19 10:19:23 +0500

    Simple

--- DataStructuresStepik/DataStructuresStepik/Program.cs
+++ DataStructuresStepik/DataStructuresStepik/Program.cs
@@ -12,16 +12,23 @@ namespace ConsoleApp1
             HashSet<string> set = new HashSet<string>();
 
             int N = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < N; i++)
+            int M = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < M; i++)
             {
                 set.Add(Console.ReadLine());
             }
 
-            int M = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < M; i++)
+            for (int i = 0; i < N - 1; i++)
             {
-                Console.WriteLine((set.Contains(Console.ReadLine())?"YES":"NO"));
+                HashSet<string> set1 = new HashSet<string>();
+                int K = Convert.ToInt32(Console.ReadLine());
+                for (int j = 0; j < K; j++)
+                {
+                    set1.Add(Console.ReadLine());
+                }
+                set.IntersectWith(set1);
             }
+            Console.WriteLine(set.Count());
         }
     }
 }
